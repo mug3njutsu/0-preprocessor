751d88295ef92e8f4d3369d091fbf9f26c1a5f9f
mug3njutsu <johngitahi272@gmail.com>
2022-06-26 23:08:24 +0300

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	write(1, "echo and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
