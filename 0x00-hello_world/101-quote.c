bd25ceff87705843bac4aec2a588916c9fbe592f
mug3njutsu <johngitahi272@gmail.com>
2022-06-27 00:32:47 +0300

    Task 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1 (Success)
+*/
+
+int main(void)
+{
+	fprint(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
