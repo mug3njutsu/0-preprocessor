5925ed1396d0ea542e12556fa00c4fed0027d706
mug3njutsu <johngitahi272@gmail.com>
2022-06-26 22:06:43 +0300

    Task 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	char string[] = "Programming is like building a multilingual puzzle"; // string initialization
+	puts(string); // write string to stdout
+	return 0;
+}
