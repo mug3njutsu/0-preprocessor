b7b515044ecdfc86fcf6d51db1eb509f4bbb3b85
mug3njutsu <johngitahi272@gmail.com>
2022-06-26 22:21:44 +0300

    Task 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(void){
+/*
+Print random string
+*/
+
+int main(void)
+{
 	char string[] = "with proper grammar, but the outcome is a piece of art,";
 	printf(string);
 	return 0;
