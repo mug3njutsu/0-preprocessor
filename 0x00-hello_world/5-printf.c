998275087e3dffd92495a236ef2d30258ddfbb35
mug3njutsu <johngitahi272@gmail.com>
2022-06-26 22:42:27 +0300

    Task 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,6 @@
 
 int main(void)
 {
-	char string[] = "with proper grammar, but the outcome is a piece of art,";
-	printf(string);
+	printf("with proper grammar, but the outcome is a piece of art,", "\n");
 	return (0);
 }
