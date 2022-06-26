4635078d364ea268d38763890e8d1c9c32b3e9ba
mug3njutsu <johngitahi272@gmail.com>
2022-06-26 22:19:47 +0300

    Task 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void){
-	char string[] = '"Programming is like building a multilingual puzzle';
+	char string[] = "\"Programming is like building a multilingual puzzle";
 	puts(string);
 	return 0;
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+	char string[] = "with proper grammar, but the outcome is a piece of art,";
+	printf(string);
+	return 0;
+}
