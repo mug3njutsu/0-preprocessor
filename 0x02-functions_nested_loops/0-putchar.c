41aac6b980b527b88e29ff33f8ff9ad64d775111
mug3njutsu <johngitahi272@gmail.com>
2022-07-05 14:55:58 +0300

    Task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
-#include "main.h" /** custom header file */
+#include "main.h" /** Custom header file */
 
 /**
- * main - prints "_putchar"
+ * main - print "_putchar"
  *
  * Return: Always 0 -> (Success)
  */
 
 int main(void)
 {
-	char out[] = "_putchar";
-	int a;
-
-	for (a < 0; a < (sizeof(out)); a++)
-	{
-		_putchar(out[i]);
-	}
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	
 	return (0);
 }
