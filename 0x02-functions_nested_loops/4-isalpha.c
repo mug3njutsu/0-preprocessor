d69b90e8dc2281d6b34c213b0002f43f0d2c005a
mug3njutsu <johngitahi272@gmail.com>
2022-07-05 16:49:14 +0300

    Task 5

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - prints 1 if c is lowercase and 0 if otherwise
+ *@c: char
+ * Return: Always 0 -> (Success)
+ */
+
+int _isalpha(int c)
+{
+return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
+}
