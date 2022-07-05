ab424ce5344af9714ce3abd5ab868d739e1ba0a5
mug3njutsu <johngitahi272@gmail.com>
2022-07-05 15:41:05 +0300

    Task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h" /** Custom header file */
+
+/**
+ * main - print alphabets "a..z"
+ *
+ * Return: Always 0 -> (Success)
+ */
+
+int main(void)
+{
+	char letters[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		print_alphabet(letters[i]);
+	}
+	return (0);
+}
