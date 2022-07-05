bf6125c6704432eb864dd65ba98105d42d9ff95d
mug3njutsu <johngitahi272@gmail.com>
2022-07-05 17:25:15 +0300

    Task 9

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
- *@n: int
+ * jack_bauer - prints every minute of the day of Jack Bauer
  * Return: validations
  */
 
