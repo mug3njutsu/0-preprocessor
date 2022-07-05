43d93b91990331fa653f53a5d4997e620194f1d3
mug3njutsu <johngitahi272@gmail.com>
2022-07-05 17:17:38 +0300

    Task 8

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+/**
+ * print_last_digit - computes the absolute value of an integer
+ *@n: int
+ * Return: validations
+ */
+
+int print_last_digit(int n)
+{
+int a;
+
+if (n < 0)
+{
+n = -n;
+}
+
+a = n % 10;
+
+if (a < 0)
+{
+a = -a;
+}
+
+_putchar(a + '0');
+
+return (a);
+}
