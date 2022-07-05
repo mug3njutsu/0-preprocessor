a54ae787034b42d44d1c190bb99812d50b6eb219
mug3njutsu <johngitahi272@gmail.com>
2022-07-05 16:00:37 +0300

    Task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+/**
+ * print_alphabet - print alphabets "a..z"
+ *
+ * Return: Always 0 -> (Success)
+ */
+
+void print_alphabet_x10(void);
+{
+    int i = 0;
+
+    while (i<10)
+    {
+        for (i = 'a'; i <= 'z'; i++)
+        {
+            _putchar(i);
+        }
+        _putchar('\n');
+    i++;
+    }
+}
