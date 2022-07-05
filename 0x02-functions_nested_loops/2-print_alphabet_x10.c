fc8bf307a5462458127509c39332d98333fb355c
mug3njutsu <johngitahi272@gmail.com>
2022-07-05 16:06:50 +0300

    Task 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,14 @@
 
 void print_alphabet_x10(void)
 {
-    int a = 0, i;
+    int a, i;
 
-    while ( a < 10 )
+    for (a = 0; a < 10; a++)
     {
         for (i = 'a'; i <= 'z'; i++)
         {
             _putchar(i);
         }
         _putchar('\n');
-        a++;
     }
 }
