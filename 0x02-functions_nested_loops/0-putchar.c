76b30a8966363f74d8f6ebf1683d103cdebb6857
mug3njutsu <johngitahi272@gmail.com>
2022-07-05 14:25:09 +0300

    Task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints "_putchar"
+ *
+ * Return: Always 0 -> (Sucess)
+ */
+
+int main(void)
+{
+	char out[] = "_putchar";
+	int i;
+
+	for (i < 0; i < (sizeof(out)); i++)
+	{
+		putchar(out[i]);
+	}
+
+	return (0);
+}
