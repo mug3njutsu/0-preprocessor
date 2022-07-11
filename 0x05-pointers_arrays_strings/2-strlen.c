b71841446c82565612fb6d360f40dab0e9d273ec
mug3njutsu <johngitahi272@gmail.com>
2022-07-11 17:17:39 +0300

    Task 3

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,13 +9,12 @@
 
 int _strlen(char *s)
 {
-int i = 0;
+int i = 0, count = 0;
 
-while (s[i] != "\0")
+while (s[i++] != "\0")
 {
-i++;
+    count += 1;
 }
 
-length = (i + 1);
-return (length);
+return (count);
 }
