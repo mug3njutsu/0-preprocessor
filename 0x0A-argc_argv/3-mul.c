86851adc467eb5e78655ac7dc3bac87831a21eae
mug3njutsu <johngitahi272@gmail.com>
2022-07-22 18:49:25 +0300

    Task 4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,14 +11,11 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
+	if (argc != 3)
 	{
-		printf("Error");
+		printf("Error\n");
+		return (1);
 	}
-	else
-	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	}
-
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
