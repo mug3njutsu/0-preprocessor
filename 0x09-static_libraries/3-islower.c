01400278c9871f93c7c9a014b93c7764127e274f
mug3njutsu <johngitahi272@gmail.com>
2022-07-23 00:32:40 +0300

    deleted

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - check lower
+ * @c: num
+ * Return: bin
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
