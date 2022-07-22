6ed0048ed32250c477ea2169f88daf82ad08ee8c
mug3njutsu <johngitahi272@gmail.com>
2022-07-22 22:50:31 +0300

    Task 5

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,40 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <string.h>
+/**
+ * num_checker - checks if a given char is number or not
+ * @a: char to be checked
+ * Return: 1, if its a number, else 0
+ **/
+int num_checker(char *a)
+{
+	int i, num, len;
+
+	i = 0;
+	num = 0;
+	len = strlen(a);
+	while (i < len)
+	{
+		if (a[i] < '0' || a[i] > '9')
+		{
+			return (-1);
+		}
+		else
+			num = num * 10 + (a[i] - '0');
+		i++;
+	}
+	return (num);
+}
 
 /**
- * main - sum of  the args
- * @argc: Argument count
- * @argv: Array of argument strings
- *
- * Return: 0 for successful exit
- */
+ * main - add positive numbers
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: result of addition or 1
+ **/
 
 int main(int argc, char *argv[])
 {
+	int i, num, sum;
 
-	int i = 0;
-	int sum = 0;
-
-	if (argc == 1)
+	sum = 0;
+	for (i = 1; i < argc; i++)
 	{
-		printf("0\n");
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
+		num = num_checker(argv[i]);
+		if (num == -1)
 		{
-			if (!isdigit(*argv[i]))
-			{
-				printf("Error\n");
-			}
-			else
-			{
-				sum += atoi(argv[i]);
-			}
+			printf("Error\n");
+			return (1);
 		}
-		printf("%d\n", sum);
+		sum += num;
 	}
-
+	printf("%d\n", sum);
 	return (0);
 }
