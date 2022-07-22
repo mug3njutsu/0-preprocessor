cb034e74fad7a32f9fa0cb77d39ac98184dce4b8
mug3njutsu <johngitahi272@gmail.com>
2022-07-23 00:25:26 +0300

    C

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two strings
+ * @dest: char
+ * @src: char
+ * @n: n bytes to src
+ * Return: stuff
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int tamanoDest = 0, l = 0;
+
+	while (*(dest + tamanoDest) != '\0')
+	{
+		tamanoDest = tamanoDest + 1;
+	}
+
+	for (l = 0; src[l] != '\0'; l++)
+	{
+		if (l < n)
+		{
+			*(dest + tamanoDest) = *(src + l);
+			tamanoDest = tamanoDest + 1;
+		}
+		else
+			break;
+	}
+	return (dest);
+}
