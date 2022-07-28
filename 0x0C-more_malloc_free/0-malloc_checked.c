ae826b197cbfa3892eebfef791dfe27369760913
mug3njutsu <johngitahi272@gmail.com>
2022-07-28 13:54:25 +0300

    Task 1

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: n bytes
+ * Return: *p -> (allocated memory)
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *allocMemo = malloc(b);
+
+	if (!allocMemo)
+	{
+		exit(98);
+	}
+
+	return (allocMemo);
+}
