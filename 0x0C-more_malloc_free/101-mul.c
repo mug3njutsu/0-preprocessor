0cb0d3c87ff9058cfaf40b393a50d1fb87774b0a
mug3njutsu <johngitahi272@gmail.com>
2022-07-28 14:40:18 +0300

    Task 6

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 int find_len(char *str);
@@ -211,9 +212,9 @@ int main(int argc, char *argv[])
 	for (index = 0; final_prod[index]; index++)
 	{
 		if (final_prod[index] != 'x')
-			_putchar(final_prod[index]);
+			putchar(final_prod[index]);
 	}
-	_putchar('\n');
+	putchar('\n');
 
 	free(next_prod);
 	free(final_prod);
