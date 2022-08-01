a09f149ad0a02ab0d56ea894d55f34d57b826af5
mug3njutsu <johngitahi272@gmail.com>
2022-08-01 21:45:07 +0300

    Task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE dog.h
 
-#include <stdlib.h>
-#include <stdio.h>
-
 /**
  * struct dog - dog struct
  * @name: dog_name
@@ -12,18 +9,15 @@
  * Return: true
  */
 
-
-typedef struct dog
+struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-}
-dog_t;
-
-
+    char *name;
+    float age;
+    char *owner;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
