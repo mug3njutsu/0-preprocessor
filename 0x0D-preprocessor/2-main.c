b8a83e8772cb32d17ec9daa50add27bbb5b274da
mug3njutsu <johngitahi272@gmail.com>
2022-08-01 21:13:01 +0300

    Task 3

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define FILE_NAME __FILE__
+
+/**
+ * main - prints the name of the file it was compiled from
+ * Return: Always 0 -> (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", FILE_NAME);
+	return (0);
+}
