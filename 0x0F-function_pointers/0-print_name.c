5e0dccabcfd67df914d902ad737803e01b2a425a
mug3njutsu <johngitahi272@gmail.com>
2022-08-03 12:49:44 +0300

    Task 1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: str
+ * @f: func
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+if (f != NULL)
+	f(name);
+}
