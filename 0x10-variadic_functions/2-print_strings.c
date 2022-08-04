04f9234c65ca90da0d348e684e098d1249ad4b49
mug3njutsu <johngitahi272@gmail.com>
2022-08-04 14:08:19 +0300

    Task 3

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print all args
+ * @separator: item to use to separate nums
+ * @n: n args to sum
+ * Return: sumof(args)
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	char *c;
+
+	va_list ap;
+
+	va_start(ap, n);
+
+	if (!separator)
+		return;
+
+	for (i = 0; i < n; i++)
+	{
+		c =  va_arg(ap,  char*);
+		printf("%s%s", c == NULL ? "(nil)" : c, i != (n - 1) ? separator : "");
+	}
+
+	va_end(ap);
+	putchar('\n');
+
+
+}
