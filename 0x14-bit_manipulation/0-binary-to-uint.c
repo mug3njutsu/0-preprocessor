6c2f9373d194dccbb5e1d51274de20f5c91564c3
mug3njutsu <johngitahi272@gmail.com>
2022-08-18 20:18:46 +0300

    Task 1

--- 0x14-bit_manipulation/0-binary-to-uint.c
+++ 0x14-bit_manipulation/0-binary-to-uint.c
@@ -1,43 +1,18 @@
 #include "main.h"
 
-/**
- * _strlen - find the len(str)
- * @s: *p -> string to check
- * Return: void
-*/
-
-int _strlen(const char *s)
-{
-int i = 0;
-while (s[i])
-	i++;
-
-return (i);
-}
-
-/**
- * binary_to_uint - converts bin to an unsigned int
- * @b: bin
- * Return: 0 or converted number
- */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = 0;
-	int i, len;
+	int i;
+	unsigned int dec_val = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	len = _strlen(b);
-
-	for (i = 0; i != len; i++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[len - i - 1] == '1')
-			n += 1 << i;
-		else if (b[len - i - 1] != '0')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-
-	return (n);
+	return (dec_val);
 }
